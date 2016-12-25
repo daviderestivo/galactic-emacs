ef3f93420a36bbefedf00c24071e00cb60515bfd
Davide Restivo <davide.restivo@swisscom.com>
2016-12-25 01:25:56 +0100

    Add melpa-stable and marmelade archives

--- init.el
+++ init.el
@@ -9,6 +9,8 @@
 ;;
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 ;;
 ;; exec-path-from-shell
