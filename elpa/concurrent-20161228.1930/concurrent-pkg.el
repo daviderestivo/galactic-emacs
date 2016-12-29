0b11f87d3dcb4a2d195856ec9eff0a5e82f0ac5b
Davide Restivo <davide.restivo@yahoo.it>
2016-12-29 20:20:16 +0100

    Install Jedi

---
+++ elpa/concurrent-20161228.1930/concurrent-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "concurrent" "20161228.1930" "Concurrent utility functions for emacs lisp" '((emacs "24.3") (deferred "0.5.0")) :url "https://github.com/kiwanami/emacs-deferred/blob/master/README-concurrent.markdown" :keywords '("deferred" "async" "concurrent"))
---
+++ elpa/deferred-20161228.1930/deferred-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "deferred" "20161228.1930" "Simple asynchronous functions for emacs lisp" '((emacs "24.3")) :url "https://github.com/kiwanami/emacs-deferred" :keywords '("deferred" "async"))
