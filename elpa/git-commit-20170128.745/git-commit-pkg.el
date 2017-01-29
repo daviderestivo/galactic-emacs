c5c55021838ea5f89e1c1122a36adabaf3edd71e
Davide Restivo <davide.restivo@yahoo.it>
2017-01-29 08:56:55 +0100

    Run auto-package-update-now

---
+++ elpa/git-commit-20170128.745/git-commit-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "git-commit" "20170128.745" "Edit Git commit messages" '((emacs "24.4") (dash "20161121.55") (with-editor "20170111.609")) :url "https://github.com/magit/magit" :keywords '("git" "tools" "vc"))
---
+++ elpa/magit-popup-20170128.745/magit-popup-pkg.el
@@ -0,0 +1,9 @@
+(define-package "magit-popup" "20170128.745" "Define prefix-infix-suffix command combos"
+  '((emacs "24.4")
+    (async "20161103.1036")
+    (dash "20161121.55"))
+  :url "https://github.com/magit/magit" :keywords
+  '("bindings"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
