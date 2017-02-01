4a8a123e15276c0dcb08bf531677db5df00135a4
Davide Restivo <davide.restivo@swisscom.com>
2017-02-01 15:11:54 +0100

    Run auto-package-update-now

---
+++ elpa/ein-20170131.844/ein-pkg.el
@@ -0,0 +1,7 @@
+(define-package "ein" "20170131.844" "Emacs IPython Notebook"
+  '((websocket "1.5")
+    (request "0.2")
+    (cl-generic "0.2")))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- elpa/request-20170120.414/request-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "request" "20170120.414" "Compatible layer for URL request in Emacs" '((emacs "24") (cl-lib "0.5")))
