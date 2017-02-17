6af577dfdd161fee994764720c25dc92e2d352b2
Davide Restivo <davide.restivo@yahoo.it>
2017-02-17 07:59:02 +0100

    Run auto-package-update-now

---
+++ elpa/magit-20170215.757/magit-pkg.el
@@ -0,0 +1,12 @@
+(define-package "magit" "20170215.757" "A Git porcelain inside Emacs"
+  '((emacs "24.4")
+    (async "20161103.1036")
+    (dash "20170207.2056")
+    (with-editor "20170111.609")
+    (git-commit "20170128.745")
+    (magit-popup "20170209.1031"))
+  :url "https://github.com/magit/magit" :keywords
+  '("git" "tools" "vc"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/magit-popup-20170214.347/magit-popup-pkg.el
@@ -0,0 +1,9 @@
+(define-package "magit-popup" "20170214.347" "Define prefix-infix-suffix command combos"
+  '((emacs "24.4")
+    (async "20161103.1036")
+    (dash "20170207.2056"))
+  :url "https://github.com/magit/magit" :keywords
+  '("bindings"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
