a3efab5a6a2cfdc657feda25a57c99422d6505f8
Davide Restivo <davide.restivo@yahoo.it>
2017-03-23 07:21:23 +0100

    Run auto-package-update-now

--- elpa/ein-20170322.650/ein-pkg.el
+++ elpa/ein-20170322.650/ein-pkg.el
@@ -1,4 +1,4 @@
-(define-package "ein" "20170320.1222" "Emacs IPython Notebook"
+(define-package "ein" "20170322.650" "Emacs IPython Notebook"
   '((websocket "1.7")
     (request "0.3")
     (cl-generic "0.3")))
