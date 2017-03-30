3539008ce1bd9a4777b426536f1ac1d7fe02851b
Davide Restivo <davide.restivo@yahoo.it>
2017-03-30 07:13:40 +0200

    Run auto-package-update-now

--- elpa/magit-20170329.221/magit-pkg.el
+++ elpa/magit-20170329.221/magit-pkg.el
@@ -1,4 +1,4 @@
-(define-package "magit" "20170322.1550" "A Git porcelain inside Emacs"
+(define-package "magit" "20170329.221" "A Git porcelain inside Emacs"
   '((emacs "24.4")
     (async "20170219.942")
     (dash "20170207.2056")
