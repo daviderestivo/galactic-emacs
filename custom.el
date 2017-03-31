2a66ab1a7282dcf429c9a9c1bd0010c64a5b16fe
Davide Restivo <davide.restivo@yahoo.it>
2017-03-31 23:23:26 +0200

    Move custom variables into custom.el

---
+++ custom.el
@@ -0,0 +1,43 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("f5512c02e0a6887e987a816918b7a684d558716262ac7ee2dd0437ab913eaec6" "4fc7f95b03416aa4270cdeefecc45f3153b8ceadef2e8d5722dce63d1bf83400" "2997ecd20f07b99259bddba648555335ffb7a7d908d8d3e6660ecbec415f6b95" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "84d2f9eeb3f82d619ca4bfffe5f157282f4779732f48a5ac1484d94d5ff5b279" "9d91458c4ad7c74cf946bd97ad085c0f6a40c370ac0a1cbeb2e3879f15b40553" "3d47d88c86c30150c9a993cc14c808c769dad2d4e9d0388a24fee1fbf61f0971" default)))
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain))
+ '(package-selected-packages
+   (quote
+    (jinja2-mode helm yaml-mode smart-mode-line-powerline-theme smart-mode-line zenburn-theme markdown-preview-mode ein jedi auto-package-update rainbow-delimiters org magit ## exec-path-from-shell)))
+ '(show-paren-mode t)
+ '(winner-mode t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-1-face ((t (:foreground "red"))))
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "orange"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "yellow"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "green"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "blue"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "violet"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "purple"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "black"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "white"))))
+ '(rainbow-delimiters-unmatched-face ((t (:background "cyan"))))
+ '(sml/col-number ((t (:inherit sml/global :background "black" :foreground "gray70" :weight bold))))
+ '(sml/filename ((t (:inherit sml/global :background "#383838" :foreground "dark gray" :weight light))))
+ '(sml/line-number ((t (:inherit sml/global :background "black" :foreground "gray70" :weight bold))))
+ '(sml/modes ((t (:inherit sml/global :background "#5F5F5F" :foreground "Black" :weight ultra-light))))
+ '(sml/modified ((t (:foreground "dark gray"))))
+ '(sml/position-percentage ((t (:inherit sml/prefix :background "#5F5F5F" :foreground "gray70" :weight normal))))
+ '(sml/prefix ((t (:inherit sml/global :background "#383838" :foreground "dark gray"))))
+ '(sml/read-only ((t (:inherit sml/not-modified :foreground "dark gray"))))
+ '(sml/vc ((t (:inherit sml/git :background "#5F5F5F" :foreground "dark gray"))))
+ '(sml/vc-edited ((t (:inherit sml/prefix :background "#5F5F5F" :foreground "burlywood1")))))
+ 
