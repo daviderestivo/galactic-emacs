691212d32c38745dd5fc6fad856a19cb1bfb4d30
Davide Restivo <davide.restivo@yahoo.it>
2017-04-08 22:49:10 +0200

    Remove markdown-preview-mode

--- elpa/markdown-mode-20170317.1202/markdown-mode-autoloads.el
+++ elpa/markdown-mode-20170317.1202/markdown-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22734 14825
+;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22761 19209
 ;;;;;;  0 0))
 ;;; Generated autoloads from markdown-mode.el
 
--- elpa/web-server-20140905.1706/web-server-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "web-server" "20140905.1706" "Emacs Web Server"
-  '((emacs "24.3"))
-  :url "https://github.com/eschulte/emacs-web-server" :keywords
-  '("http" "server" "network"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
