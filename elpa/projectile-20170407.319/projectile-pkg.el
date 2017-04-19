f91d4c76d4bd3b84089aba9def4138638032e27d
Davide Restivo <davide.restivo@yahoo.it>
2017-04-19 06:26:12 +0200

    Run auto-package-update-now

--- elpa/projectile-20170407.319/projectile-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "projectile" "20170407.319" "Manage and navigate projects in Emacs easily" '((emacs "24.1") (pkg-info "0.4")) :url "https://github.com/bbatsov/projectile" :keywords '("project" "convenience"))
