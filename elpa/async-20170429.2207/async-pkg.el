f0ddc76257fce687e30d40c79fb3b325be84d79d
Davide Restivo <davide.restivo@yahoo.it>
2017-05-01 09:05:27 +0200

    Run auto-package-update-now

--- elpa/async-20170429.2207/async-pkg.el
+++ elpa/async-20170429.2207/async-pkg.el
@@ -1,4 +1,4 @@
-(define-package "async" "20170219.942" "Asynchronous processing in Emacs" 'nil :keywords
+(define-package "async" "20170429.2207" "Asynchronous processing in Emacs" 'nil :keywords
   '("async")
   :url "https://github.com/jwiegley/emacs-async")
 ;; Local Variables:
