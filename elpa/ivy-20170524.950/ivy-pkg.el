05b007d6e92c4e0f6bcdc421ea46c4560e548933
Davide Restivo <davide.restivo@yahoo.it>
2017-05-26 10:57:35 +0200

    Run auto-package-update-now

---
+++ elpa/ivy-20170524.950/ivy-pkg.el
@@ -0,0 +1,7 @@
+(define-package "ivy" "20170524.950" "Incremental Vertical completYon"
+  '((emacs "24.1"))
+  :url "https://github.com/abo-abo/swiper" :keywords
+  '("matching"))
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/markdown-mode-20170525.435/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20170525.435" "Major mode for Markdown-formatted text" '((emacs "24") (cl-lib "0.5")) :commit "cf0438d9a9f59344d7c43265160dc69f3e2ca5ed" :url "http://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
