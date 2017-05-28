3aa84fb03a0f57a81f0039a8d1547ffca5a5d15d
Davide Restivo <davide.restivo@yahoo.it>
2017-05-28 08:38:23 +0200

    Run auto-package-update-now

--- elpa/helm-20170526.2256/helm-pkg.el
+++ elpa/helm-20170526.2256/helm-pkg.el
@@ -1,4 +1,4 @@
-(define-package "helm" "20170526.36" "Helm is an Emacs incremental and narrowing framework"
+(define-package "helm" "20170526.2256" "Helm is an Emacs incremental and narrowing framework"
   '((emacs "24.4")
     (async "1.9.2")
     (popup "0.5.3")
