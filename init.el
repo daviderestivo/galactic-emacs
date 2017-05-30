19f9344e8cded3fe4de9e88c41c307a64e4dee4f
Davide Restivo <davide.restivo@yahoo.it>
2017-05-30 08:13:17 +0200

    Add copy-line function

--- init.el
+++ init.el
@@ -510,3 +510,30 @@
 ;; Enable octave-mode for .m files
 ;;
 (add-to-list 'auto-mode-alist '("\\.m\\'" . octave-mode))
+
+;;
+;; copy-line - Source https://www.emacswiki.org/emacs/CopyingWholeLines
+;;
+(defun copy-line (arg)
+  "Copy lines (as many as prefix argument) in the kill ring.
+    Ease of use features:
+     - Move to start of next line.
+     - Appends the copy on sequential calls.
+     - Use newline as last char even on the last line of the buffer.
+     - If region is active, copy its lines."
+  (interactive "p")
+  (let ((beg (line-beginning-position))
+        (end (line-end-position arg)))
+    (when mark-active
+      (if (> (point) (mark))
+          (setq beg (save-excursion (goto-char (mark)) (line-beginning-position)))
+        (setq end (save-excursion (goto-char (mark)) (line-end-position)))))
+    (if (eq last-command 'copy-line)
+        (kill-append (buffer-substring beg end) (< end beg))
+      (kill-ring-save beg end)))
+  (kill-append "\n" nil)
+  (beginning-of-line (or (and arg (1+ arg)) 2))
+  (if (and arg (not (= 1 arg))) (message "%d lines copied" arg)))
+
+;; copy-line key binding
+(global-set-key "\C-c\C-c" 'copy-line)
