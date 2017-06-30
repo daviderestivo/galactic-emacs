bb92e12b84d4585701fa11185accdea5b19ccd1e
Davide Restivo <davide.restivo@yahoo.it>
2017-06-30 02:03:33 +0200

    Use DejaVu Sans Mono instead of Menlo as default font

--- custom.el
+++ custom.el
@@ -18,5 +18,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 140 :family "Menlo"))))
+ '(default ((t (:height 140 :family "DejaVu Sans Mono"))))
  '(whitespace-line ((t (:background "gray20" :foreground "dark gray")))))
