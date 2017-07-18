8de178e1ef3a64061ad14f41e892c901fdd17205
Davide Restivo <davide.restivo@yahoo.it>
2017-07-18 14:24:17 +0200

    Some minor cosmetic changes

--- init.el
+++ init.el
@@ -503,15 +503,15 @@ The following %-sequences are provided:
 (use-package org
   :ensure org-plus-contrib
   :config
-  ;; ORG directories and files location
+  ;; ORG directories and files
   (setq org-directory "~/org/")
   (setq org-agenda-files (list (concat org-directory "agenda")))
   (setq org-default-notes-file (concat org-directory "refile.org"))
-  ;; Set org ellipsis style to a downward arrow instead of "..."
+  ;; Set org ellipsis style to a downward arrow "⤵" instead of "..."
   (setq org-ellipsis "⤵")
   ;; Save all org buffers every hour
   (run-at-time "00:59" 3600 'org-save-all-org-buffers)
-  ;; Add CLOSED: [timestamp] when a task is marked as DONE
+  ;; Add "CLOSED: [timestamp]" when a task is marked as DONE
   (setq org-log-done t)
   ;; Equivalent of "#+STARTUP: showeverything " on all ORG files
   (setq org-startup-folded nil)
