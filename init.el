7f64b63e01920537d309f13155eb09fae5700df5
Davide Restivo <davide.restivo@yahoo.it>
2017-11-12 16:23:26 +0100

    New Release 4.3.0

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 ;; Author: Davide Restivo <davide.restivo@yahoo.it>
 ;; Maintainer: Davide Restivo <davide.restivo@yahoo.it>
 ;; URL: https://github.com/daviderestivo/emacs-config
-;; Version: 4.2.0
+;; Version: 4.3.0
 ;; Keywords: emacs config dotemacs
 
 
