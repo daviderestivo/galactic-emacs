42a1d3c409d3c790a48bdd3b5db0d84d4228e596
Davide Restivo <davide.restivo@yahoo.it>
2017-12-02 16:59:31 +0100

    Improve ensure-system-package compatibility with GNU/Linux.
    Add apt-get command in case brew is unavailable, that would mean we
    are most probably running GNU/Linux.

--- init.el
+++ init.el
@@ -1112,7 +1112,7 @@ This function requires `all-the-icons' package to be installed
 ;; On macOS use: 'brew install the_silver_searcher'
 (use-package helm-ag
   :ensure t
-  :ensure-system-package (ag . "brew install the_silver_searcher")
+  :ensure-system-package (ag . "brew install the_silver_searcher || sudo apt-get install the_silver_searcher")
   :config
   ;; Use .agignore file at project root
   (setq helm-ag-use-agignore t)
@@ -1280,7 +1280,7 @@ This function requires `all-the-icons' package to be installed
 ;; Please look at: https://blogs.fsfe.org/jens.lechtenboerger/2014/03/23/certificate-pinning-for-gnu-emacs/
 (use-package gnutls
   :ensure t
-  :ensure-system-package (gnutls-cli . "brew install gnutls")
+  :ensure-system-package (gnutls-cli . "brew install gnutls || sudo apt-get install gnutls")
   :config
   (setq tls-program '("gnutls-cli --strict-tofu -p %p %h")
         imap-ssl-program '("gnutls-cli --strict-tofu -p %p %s")
