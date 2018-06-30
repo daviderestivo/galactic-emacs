bcf68607ca28759598c9baab81c33d5526e83db6
Davide Restivo <davide.restivo@yahoo.it>
2018-06-30 21:11:22 +0200

    Bump version to 5.0

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 ;; Author: Davide Restivo <davide.restivo@yahoo.it>
 ;; Maintainer: Davide Restivo <davide.restivo@yahoo.it>
 ;; URL: https://github.com/daviderestivo/emacs-config
-;; Version: 4.3.0
+;; Version: 5.0.0
 ;; Keywords: emacs config dotemacs
 
 
