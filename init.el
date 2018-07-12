f51f0f5b2813caa8d5ba3308820e4a4f1bc3fa6c
Davide Restivo <davide.restivo@yahoo.it>
2018-07-12 16:29:13 +0200

    Add .knd to auto-mode-alist

--- init.el
+++ init.el
@@ -1102,7 +1102,7 @@ User Interface (GUI). This function has to be invoked:
   :defer t
   :ensure t
   :config
-  (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+  (add-to-list 'auto-mode-alist '("\\.\\(yml\\|knd)\\)\\'" . yaml-mode))
   (add-hook 'yaml-mode-hook
             '(lambda ()
                (define-key yaml-mode-map "\C-m" 'newline-and-indent)
