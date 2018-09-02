96f4379245d32ffa88e8c9554770356ae178e9f8
Davide Restivo <davide.restivo@yahoo.it>
2018-09-02 12:12:27 +0200

    Add cosmetic changes

--- init.el
+++ init.el
@@ -1100,13 +1100,13 @@ User Interface (GUI). This function has to be invoked:
   :ensure t
   :ensure-system-package (gvgen . graphviz)
   :config
-  (setq org-mind-map-engine "dot")       ; Default; Directed Graph
-  ;; (setq org-mind-map-engine "neato")  ; Undirected Spring Graph"
-  ;; (setq org-mind-map-engine "twopi")  ; Radial Layout"
-  ;; (setq org-mind-map-engine "fdp")    ; Undirected Spring Force-Directed"
+  (setq org-mind-map-engine "dot")       ; Default. Directed Graph
+  ;; (setq org-mind-map-engine "neato")  ; Undirected Spring Graph
+  ;; (setq org-mind-map-engine "twopi")  ; Radial Layout
+  ;; (setq org-mind-map-engine "fdp")    ; Undirected Spring Force-Directed
   ;; (setq org-mind-map-engine "sfdp")   ; Multiscale version of fdp for the layout of large graphs
   ;; (setq org-mind-map-engine "twopi")  ; Radial layouts
-  ;; (setq org-mind-map-engine "circo")  ; Circular Layout"
+  ;; (setq org-mind-map-engine "circo")  ; Circular Layout
   )
 
 ;; rainbow-delimiters
