3da002c28585889113b0b5610ee387da2261a690
Davide Restivo <davide.restivo@yahoo.it>
2018-10-09 09:50:26 +0200

    Small fix on custom.el

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("b013ed8ee4f85ecd576753ee81bba3e00e0ed49edc010fad34d440788e3253f2" "afed43834a750c04f9a1d914873d61c76f444c7e8d0139ed230a5308d9649f86" default))
+   '("ce1a0a82a5735a962cbf26f8bc513676d186c408cbf2423ab7dff052ff70e3d5" "b013ed8ee4f85ecd576753ee81bba3e00e0ed49edc010fad34d440788e3253f2" "afed43834a750c04f9a1d914873d61c76f444c7e8d0139ed230a5308d9649f86" default))
  '(package-selected-packages
    '(json-snatcher json-reformat json-mode package-lint treepy graphql dired-sidebar vscode-icon org-mind-map avy helm-github-stars eyebrowse dashboard bug-hunter magit-org-todos cider imenu-list ibuffer-sidebar multiple-cursors auto-sudoedit esh-autosuggest shell-pop dockerfile-mode command-log-mode yang-mode org-beautify-theme all-the-icons nlin-hl nlinum-hl diff-hl org-bullets which-key wttrin volatile-highlights helm-ag org-download helm-projectile psession undo-tree helm-descbinds org-plus-contrib ob-ipython helm-config py-autopep8 elpy magit markdown-mode atom-one-dark-theme projectile company jinja2-mode helm yaml-mode smart-mode-line auto-package-update rainbow-delimiters exec-path-from-shell diminish use-package-ensure-system-package use-package)))
 
