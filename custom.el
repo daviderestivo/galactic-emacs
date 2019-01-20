e41dd71bb738cb8fe7cd3797ff87e48229132f92
Davide Restivo <davide.restivo@yahoo.it>
2019-01-20 09:37:03 +0100

    Cosmetic changes to custom.el

--- custom.el
+++ custom.el
@@ -4,9 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("bf5bdab33a008333648512df0d2b9d9710bdfba12f6a768c7d2c438e1092b633" "bc75dfb513af404a26260b3420d1f3e4131df752c19ab2984a7c85def9a2917e" default))
+   (quote
+    ("bf5bdab33a008333648512df0d2b9d9710bdfba12f6a768c7d2c438e1092b633" "bc75dfb513af404a26260b3420d1f3e4131df752c19ab2984a7c85def9a2917e" default)))
  '(package-selected-packages
-   '(idle-org-agenda smartparens geiser load-bash-alias smart-mode-line-atom-one-dark-theme json-snatcher json-reformat json-mode package-lint treepy graphql dired-sidebar vscode-icon org-mind-map avy helm-github-stars eyebrowse dashboard bug-hunter magit-org-todos cider imenu-list ibuffer-sidebar multiple-cursors auto-sudoedit esh-autosuggest shell-pop dockerfile-mode command-log-mode yang-mode org-beautify-theme all-the-icons nlin-hl nlinum-hl diff-hl org-bullets which-key wttrin volatile-highlights helm-ag org-download helm-projectile psession undo-tree helm-descbinds org-plus-contrib ob-ipython helm-config py-autopep8 elpy magit markdown-mode atom-one-dark-theme projectile company jinja2-mode helm yaml-mode smart-mode-line auto-package-update rainbow-delimiters exec-path-from-shell diminish use-package-ensure-system-package use-package)))
+   (quote
+    (idle-org-agenda smartparens geiser load-bash-alias smart-mode-line-atom-one-dark-theme json-snatcher json-reformat json-mode package-lint treepy graphql dired-sidebar vscode-icon org-mind-map avy helm-github-stars eyebrowse dashboard bug-hunter magit-org-todos cider imenu-list ibuffer-sidebar multiple-cursors auto-sudoedit esh-autosuggest shell-pop dockerfile-mode command-log-mode yang-mode org-beautify-theme all-the-icons nlin-hl nlinum-hl diff-hl org-bullets which-key wttrin volatile-highlights helm-ag org-download helm-projectile psession undo-tree helm-descbinds org-plus-contrib ob-ipython helm-config py-autopep8 elpy magit markdown-mode atom-one-dark-theme projectile company jinja2-mode helm yaml-mode smart-mode-line auto-package-update rainbow-delimiters exec-path-from-shell diminish use-package-ensure-system-package use-package))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
