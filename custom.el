c5dac7db5d4b71ba3e1f1bc8476668fe5d4981a0
Davide Restivo <davide.restivo@yahoo.it>
2019-06-12 09:45:49 +0200

    Update atom-one-dark theme

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("07140e89bd4e9a0aecca04a24a1ad124370972382addded680c41d58e749907d" "bc75dfb513af404a26260b3420d1f3e4131df752c19ab2984a7c85def9a2917e" default))
+   '("cb9b1c007e61e0802b5ffe3df86188eded1d1cbd9f93a461130debc46bb4e1ac" "bc75dfb513af404a26260b3420d1f3e4131df752c19ab2984a7c85def9a2917e" default))
  '(package-selected-packages
    '(treemacs-magit auto-highlight-symbol beacon open-junk-file move-text highlight-indent-guides goto-line-preview string-inflection dap-mode lsp-java lsp-ui company-lsp lsp-mode treemacs-icons-dired treemacs-projectile treemacs smartparens geiser load-bash-alias json-mode json-snatcher json-reformat package-lint avy helm-github-stars dashboard eyebrowse bug-hunter cider imenu-list ibuffer-sidebar multiple-cursors auto-sudoedit esh-autosuggest shell-pop dockerfile-mode command-log-mode yang-mode diff-hl which-key wttrin volatile-highlights psession undo-tree markdown-mode elpy py-autopep8 company helm-projectile helm-descbinds helm-ag helm jinja2-mode yaml-mode smart-mode-line-atom-one-dark-theme smart-mode-line auto-package-update rainbow-delimiters idle-org-agenda org-mind-map org-beautify-theme org-bullets org-download ob-ipython org-plus-contrib magit-org-todos magit exec-path-from-shell atom-one-dark-theme all-the-icons diminish use-package-ensure-system-package)))
 
