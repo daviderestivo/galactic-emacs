81932358f699c74522b1cab9d28d436f851d4c70
Davide Restivo <davide.restivo@yahoo.it>
2019-07-06 20:18:14 +0200

    Create empty custom file if it does not exists

--- init.el
+++ init.el
@@ -201,6 +201,9 @@
 ;; Save custom variables to custom.el
 (setq custom-file (expand-file-name "custom.el"
                                     user-emacs-directory))
+;; Create empty custom file if it does not exists
+(unless (file-exists-p custom-file)
+  (write-region "" nil custom-file))
 (when (file-exists-p custom-file)
   (load custom-file 'noerror))
 
