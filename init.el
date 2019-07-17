d763e8e7e7aac0cc9716343b394c96ef12d97b93
Davide Restivo <davide.restivo@yahoo.it>
2019-07-17 22:56:00 +0200

    Re-enable https for elpa.gnu.org

--- init.el
+++ init.el
@@ -156,7 +156,14 @@
       '(("melpa" . "http://melpa.org/packages/")
         ("melpa-stable" . "https://stable.melpa.org/packages/")
         ("org" . "https://orgmode.org/elpa/")
-        ("gnu" . "http://elpa.gnu.org/packages/")))
+        ("gnu" . "https://elpa.gnu.org/packages/")))
+
+;; Some combination of GNU TLS and Emacs fail to retrieve archive
+;; contents over https.
+;; https://www.reddit.com/r/emacs/comments/cdei4p/failed_to_download_gnu_archive_bad_request/etw48ux
+;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=34341
+(if (and (version< emacs-version "26.3") (>= libgnutls-version 30600))
+    (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"))
 
 ;; Change the below priorities if you prefer melpa-stable packages.
 ;; Higher is better.
