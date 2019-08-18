120051af9975ca7445084d5feecf12e0dc83f765
Davide Restivo <davide.restivo@yahoo.it>
2019-08-18 18:05:18 +0200

    Increase gc-cons-threshold values

--- init.el
+++ init.el
@@ -131,16 +131,16 @@
 ;;
 ;; Avoid garbage collection during startup. The GC eats up quite a bit
 ;; of time, easily doubling the startup time. The trick is to turn up
-;; the memory threshold (500 MB should be sufficient) in order to
+;; the memory threshold (512 MB should be sufficient) in order to
 ;; prevent it from running during startup.
-(setq gc-cons-threshold (* 500 1024 1024)
+(setq gc-cons-threshold (* 512 1024 1024 1024)
       gc-cons-percentage 0.6)
 
 ;; After Emacs startup has been completed, set `gc-cons-threshold' to
-;; 16 MB and reset `gc-cons-percentage' to its original value.
+;; 128 MB and reset `gc-cons-percentage' to its original value.
 (add-hook 'emacs-startup-hook
           (lambda ()
-            (setq gc-cons-threshold (* 16 1024 1024)
+            (setq gc-cons-threshold (* 128 1024 1024)
                   gc-cons-percentage 0.1)))
 
 ;; Log Emacs startup time in *Messages*
