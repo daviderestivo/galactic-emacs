f1f0ee067896e3527eaf1901e733eb331caf1ba2
Davide Restivo <davide.restivo@yahoo.it>
2019-09-05 06:26:04 +0200

    Bump comware-router-mode version

--- lisp/comware-router-mode.el
+++ lisp/comware-router-mode.el
@@ -4,15 +4,16 @@
 
 ;; Author: Davide Restivo <davide.restivo@yahoo.it>
 ;; Maintainer: Davide Restivo <davide.restivo@yahoo.it>
-;; Version: 0.1
+;; Created: 25 Jul 2019
+;; Version: 0.2
 ;; URL: https://github.com/daviderestivo/comware-router-mode
-;; Package-Requires: ((emacs "24.3"))
+;; Package-Requires: ((dash "2.16.0") (emacs "24"))
 ;; Keywords: convenience faces
 
 ;;; Commentary:
 ;;
-;;  A major mode for editing Comware router and switches configuration
-;;  file.
+;;  A major mode for editing Comware router and switch configuration
+;;  files.
 
 ;;; License:
 ;;
@@ -31,19 +32,24 @@
 
 ;;; Change Log:
 ;; 0.1 - 2019/07/25 - First version
+;; 0.2 - 2019/08/04 - Add VRFs, interfaces, route-policies listing functions
 
 ;;; Code:
+(require 'dash)
 
 ;; Hook
 (defvar comware-router-mode-hook nil
   "Hook called by \"comware-router-mode\"")
 
-(defvar comware-router-mode-map
-  (let
-      ((comware-router-mode-map (make-keymap)))
-    (define-key comware-router-mode-map "\C-j" 'newline-and-indent)
-    comware-router-mode-map)
-  "Keymap for Comware router configuration major mode")
+;; Mode map
+(defvar comware-router-mode-map nil
+  "Keymap for Keymap for Comware router configuration major mode")
+(progn
+  (setq comware-router-mode-map (make-sparse-keymap))
+  (define-key comware-router-mode-map (kbd "C-j")       'newline-and-indent)
+  (define-key comware-router-mode-map (kbd "C-c C-l v") 'comware-router-vrf-list)
+  (define-key comware-router-mode-map (kbd "C-c C-l i") 'comware-router-interfaces-list)
+  (define-key comware-router-mode-map (kbd "C-c C-l r") 'comware-router-route-policies-list))
 
 ;; Font locking definitions
 (defvar comware-router-ipadd-face 'comware-router-ipadd-face "Face for IP addresses")
@@ -144,17 +150,101 @@
   "Indent current line as comware router config line"
   (indent-relative-first-indent-point))
 
+;; Regexp match
+(defun comware-router--match-regexp-in-buffer (regexp regexp-group)
+  "Return a list of cons cells matching REGEXP and the given REGEXP-GROUP.
+The returned list's elements have the following structure:
+
+(POS . TEXT-PLIST)
+
+E.g.:
+((261 . #(\"VRF-1\" 0 5 (fontified t face font-lock-function-name-face)))
+ (423 . #(\"VRF-2\" 0 5 (fontified t face font-lock-function-name-face))))
+
+where POS is the position of the match in the original buffer,
+and TEXT-PLIST is the matched string with faces information."
+  (interactive)
+  (let ((matches)
+        (-compare-fn '(lambda (ele1 ele2)
+                        (equal
+                         (substring-no-properties (cdr ele1))
+                         (substring-no-properties (cdr ele2))))))
+    (save-match-data
+      (save-excursion
+        (with-current-buffer (current-buffer)
+          (save-restriction
+            (widen)
+            (beginning-of-buffer)
+            (while (search-forward-regexp regexp nil t 1)
+              (push `(,(point) . ,(match-string regexp-group)) matches))))))
+    (reverse (-sort -compare-fn (-uniq matches)))))
+
+(defun comware-router--write-output-in-destination-buffer (text-plists source-buffer destination-buffer)
+  "Write TEXT-PLISTS in DESTINATION-BUFFER, creating an hyperlink
+to SOURCE-BUFFER for each entry."
+  (set-buffer
+   (get-buffer-create destination-buffer))
+  (dolist (ele text-plists)
+    (comware-router--insert-text-button ele source-buffer))
+  (set-buffer-modified-p nil)
+  (split-window-below)
+  (other-window 0)
+  (switch-to-buffer destination-buffer)
+  (local-set-key (kbd "q") (lambda () (interactive)
+                             (kill-this-buffer)
+                             (delete-window))))
+
+(defun comware-router--insert-text-button (text-plist source-buffer)
+  "Create a text button for TEXT-PLIST with an hyperlink to the
+corresponding text position in SOURCE-BUFFER"
+  (insert-button (substring-no-properties (cdr text-plist))
+                 'source-buffer source-buffer
+                 'text-plist text-plist
+                 'action (lambda (x)
+                           (print (button-get x 'source-buffer))
+                           (switch-to-buffer-other-window (button-get x 'source-buffer))
+                           (goto-char (car (button-get x 'text-plist)))))
+  (newline))
+
+;; Show VRFs in new buffer
+(defun comware-router-vrf-list ()
+  "List all VRFs in a new buffer called \"*Comware: vpn-instances*\""
+  (interactive)
+  (comware-router--write-output-in-destination-buffer
+   (comware-router--match-regexp-in-buffer "^ip vpn-instance \\(.*\\)" 1) ; Matches
+   (buffer-name (current-buffer)) ; Source buffer
+   "*Comware: vpn-instances*"))   ; Destination buffer
+
+;; Show route policies in new buffer
+(defun comware-router-route-policies-list ()
+  "List all route policies in a new buffer called \"*Comware: route-policies*\""
+  (interactive)
+  (comware-router--write-output-in-destination-buffer
+   (comware-router--match-regexp-in-buffer "^route-policy \\([[:alnum:]][[:graph:]]*\\) \\(.*\\)" 1) ; Matches
+   (buffer-name (current-buffer))  ; Source buffer
+   "*Comware: route-policies*"))   ; Destination buffer
+
+;; Show interfaces in new buffer
+(defun comware-router-interfaces-list ()
+  "List all interfaces in a new buffer called \"*Comware: interfaces*\""
+  (interactive)
+  (comware-router--write-output-in-destination-buffer
+   (comware-router--match-regexp-in-buffer "^interface \\(.*\\)" 1) ; Matches
+   (buffer-name (current-buffer)) ; Source buffer
+   "*Comware: interfaces*"))      ; Destination buffer
+
 ;; Custom syntax table
 (defvar comware-router-mode-syntax-table (make-syntax-table)
   "Syntax table for comware router mode")
-(modify-syntax-entry ?_  "w" comware-router-mode-syntax-table) ;All _'s are part of words.
-(modify-syntax-entry ?:  "w" comware-router-mode-syntax-table) ;All :'s are part of words.
-(modify-syntax-entry ?-  "w" comware-router-mode-syntax-table) ;All -'s are part of words.
-(modify-syntax-entry ?#  "<" comware-router-mode-syntax-table) ;All #'s start comments.
-(modify-syntax-entry ?\n ">" comware-router-mode-syntax-table) ;All newlines end comments.
-(modify-syntax-entry ?\r ">" comware-router-mode-syntax-table) ;All linefeeds end comments.
+(modify-syntax-entry ?_  "w" comware-router-mode-syntax-table) ; All _'s are part of words.
+(modify-syntax-entry ?-  "w" comware-router-mode-syntax-table) ; All -'s are part of words.
+(modify-syntax-entry ?:  "w" comware-router-mode-syntax-table) ; All :'s are part of words.
+(modify-syntax-entry ?#  "<" comware-router-mode-syntax-table) ; All #'s start comments.
+(modify-syntax-entry ?\n ">" comware-router-mode-syntax-table) ; All newlines end comments.
+(modify-syntax-entry ?\r ">" comware-router-mode-syntax-table) ; All linefeeds end comments.
 
 ;; Entry point
+;;;###autoload
 (defun comware-router-mode  ()
   "Major mode for editing Comware routers/switches configuration files"
   (interactive)
