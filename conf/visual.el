a1d0dc98c18a527fca6a1b5b16e45cd2519c2b57
Davide Restivo <davide.restivo@yahoo.it>
2021-10-21 14:58:23 +0200

    Change treemacs sorting

--- conf/visual.el
+++ conf/visual.el
@@ -513,7 +513,7 @@
           treemacs-show-hidden-files          t
           treemacs-silent-filewatch           nil
           treemacs-silent-refresh             nil
-          treemacs-sorting                    'alphabetic-desc
+          treemacs-sorting                    'alphabetic-asc
           treemacs-space-between-root-nodes   t
           treemacs-tag-follow-cleanup         t
           treemacs-tag-follow-delay           1.5
