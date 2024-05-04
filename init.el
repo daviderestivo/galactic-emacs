9b76e66d1bb4c0954fe4d41f320d6ea523e5eb55
Davide Restivo <davide.restivo@yahoo.it>
2024-05-04 09:12:01 +0200

    [CI] Fix package signature verification error

--- init.el
+++ init.el
@@ -128,14 +128,14 @@
   :init
   ;; The below assumes gpg is installed in `/usr/local/bin'
   (custom-set-variables '(epg-gpg-program  "/usr/local/bin/gpg"))
-  ;; Enable packages signature verification only if the current Emacs
-  ;; instance is not a CI one (Continuous Integration) and GPG
-  ;; is available
-  (if (boundp 'galactic-emacs-ci)
-      (setq package-check-signature nil)
-    (setq package-check-signature (when (executable-find "gpg") 'allow-unsigned)))
+  (setq package-check-signature (when (executable-find "gpg") 'allow-unsigned))
   :config
-  (gnu-elpa-keyring-update))
+  ;; If your keys are already too old, causing signature verification
+  ;; errors when installing packages, then in order to install this
+  ;; package you have to temporarily disable signature verification
+  ;; (see variable `package-check-signature') :-(
+  (let ((package-check-signature nil))
+    (gnu-elpa-keyring-update)))
 
 ;; system-packages
 (use-package system-packages
